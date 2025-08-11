ac0d89d9641042c16ebc3822d9dfe20d1f4274bd
RyoKawasima <ryokawasima@RyoKawasimanoMacBook-Air.local>
2025-08-11 16:29:12 +0900

    アカウント編集ページへのリンクを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ↓↓↓ この3行を追加する ↓↓↓
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # :sign_up（新規登録）の時に、:name というキーのパラメータを許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
